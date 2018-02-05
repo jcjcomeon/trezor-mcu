f10212e96fe1d6e861cb838e3e50aa784dcb1e10
jcjcomeon <jcjcomeon@163.com>
2018-02-05 18:03:09 +0800

    fix error

--- layout.h
+++ layout.h
@@ -28,6 +28,7 @@
 #define ZHLINE  13
 
 void layoutDialog(const BITMAP *icon, const char *btnNo, const char *btnYes, const char *desc, const char *line1, const char *line2, const char *line3, const char *line4, const char *line5, const char *line6);
+void layoutZhDialog(const BITMAP *icon, const char *btnNo, const char *btnYes, const char *desc, const char *line1, const char *line2, const char *line3, const char *line4);
 void layoutProgressUpdate(bool refresh);
 void layoutProgress(const char *desc, int permil);
 
